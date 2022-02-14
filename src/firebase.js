f267049cf55f18103188ce1a3d48f8ae1ea4931a
tea551234 <tea551234@gmail.com>
2022-02-14 15:25:04 +0800

    20202141525

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/auth'
+
+
+const app = firebase.initializeApp({
+
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    databaseURL:process.env.REACT_APP_FIREBASE_DATABASE_URL,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECTID_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDERID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID
+
+
+
+})
+
+export const auth = app.auth()
+export default app
